4022aa50ecb487d02e1064f665b347f1a5da6dc0
hauleguitare <alexander.dutyone@gmail.com>
2022-11-23 13:18:33 +0700

    add(): Adding Component into App component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,42 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import FormInput, { ITodoList } from './components/form-input/FormInput.vue'
+import ListTodo from './components/list-todo/ListTodo.vue'
+
+
+const listTodo = ref<ITodoList[]>([]);
+
+function handleSubmit(value: any){
+    console.log(value);
+    listTodo.value.push(value);
+}
+
+function handleRemove(item_id: number){
+    const findIdx = listTodo.value.findIndex((value) => value.id === item_id);
+    if (findIdx === -1){
+        return;
+    }
+    listTodo.value.splice(findIdx, 1);
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <p class="title-list">TODO LIST with VueJS</p>
+    <FormInput @submit-item="handleSubmit"/>
+    <ListTodo :data="listTodo" @remove-item="handleRemove"/>
+
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
+.title-list {
+    font-size: 30px
+}
+
 .logo {
   height: 6em;
   padding: 1.5em;
