78edd75c409262f5a73fea6c87b78808f034c46e
hauleguitare <alexander.dutyone@gmail.com>
2022-11-23 13:18:07 +0700

    add(): Create list todo

---
+++ src/components/list-todo/ListTodo.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul v-if="props.data">
+        <li v-for="item of props.data" :key="item.id">
+            <button @click.left="onRemove(item.id)">
+                <span>
+                    {{item.title}}
+                </span>
+            </button>
+        </li>
+    </ul>
+</template>
+<script setup lang="ts">
+import { ITodoList } from '../form-input/FormInput.vue';
+
+    export interface IListTodoProps{
+        data?: ITodoList[]
+    }
+    const props = withDefaults(defineProps<IListTodoProps>(), {});
+    const emit = defineEmits(['remove-item']);
+
+    function onRemove(item_id: number){
+        emit('remove-item', item_id);
+    }
+
+</script>
+<!-- <style scoped>
+    .finish-todo {
+        text-decoration: line-through;
+        font-style: italic;
+    }
+</style> -->
